07e08927285b973df6ac9b122b8c35417b78f091
BinaryRx <Binaryrx@gmail.com>
2019-08-25 00:21:39 +0300

    Fixed disappearing filter on cars after filtration

--- src/copy/index.php
+++ src/copy/index.php
@@ -101,16 +101,6 @@ if($utm_source){
   <script async src="js/gtm.js"></script>
   <script async src="js/ga.js"> </script>
 
-
-
-<!-- <script> -->
-  <!-- window.mobilecheck = function() {
-    var check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-  }; -->
-<!-- </script> -->
-
   </head>
 
 
@@ -203,14 +193,6 @@ if($utm_source){
                 <h3 id="carsHeader">הרכבים שבחרתי</h3>
                 <div class="formCarsCont" id="formCars">
 
-                    <!-- <div class="formSelectedCar">
-                      <div class="inner">
-                        <div>
-                          SKODA NEW SUPERB
-                          <a class="removeSelection"><img src="images/desktop/X_icon.png" alt=""></a>
-                        </div>
-                      </div>
-                    </div> -->
 
                 </div>
                 <button id="sendME">שלח ></button>
@@ -280,90 +262,6 @@ if($utm_source){
         </div>
 
         <div class="selection__cars cars">
-
-          <!-- REQUESTED STARTING CARS  -->
-
-          <!-- FAMILY -->
-          <!--
-            <div class="car default" data-car-name="Octavia" data-car-brand="skoda" style="background:url('images/cars_bg/skoda_octavia.jpg') ">
-
-              <div class="content">Octavia</div>
-            </div>
-
-            <div class="car default" data-car-name="Golf" data-car-brand="vw" style="background:url('images/cars_bg/vw_golf.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="A3" style="background:url('images/cars_bg/audi_a3.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Leon" style="background:url('images/cars_bg/seat_leon.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-
-            SUV
-            <div class="car default" data-car-name="Kodiaq" style="background:url('images/cars_bg/skoda_kodiaq.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Tiguan" style="background:url('images/cars_bg/vw_tiguan.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Q3" style="background:url('images/cars_bg/audi_q3.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Ateca" style="background:url('images/cars_bg/seat_ateca.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-
-            <!-- Managers
-            <div class="car default" data-car-name="Superb" style="background:url('images/cars_bg/skoda_superb.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car" data-car-name="tiguan" style="background:url('images/cars_bg/vw_tiguan.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="A4" style="background:url('images/cars_bg/audi_a4.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-
-            Compact
-            <div class="car default" data-car-name="Fabia" style="background:url('images/cars_bg/skoda_fabia.jpg')">
-              <div class="content">לא כולל אגרת רישוי בסך X,XXX₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Polo" style="background:url('images/cars_bg/vw_polo.jpg') ">
-              <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="A1" style="background:url('images/cars_bg/audi_a1.jpg') ">
-            <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            <div class="car default" data-car-name="Ibiza" style="background:url('images/cars_bg/seat_ibiza.jpg') ">
-            <div class="content">לא כולל אגרת רישוי בסך 1,355₪</div>
-            </div>
-
-            -->
-
-
-
-
-
-
-
-
-
-
-
           </div>
 
           <footer id="foot">
@@ -383,9 +281,9 @@ if($utm_source){
 
     </script>
 
-    <script src="js/zoom.min.js?v=1.1"></script>
-    <script src="js/formValidationAjax.min.js?v=1.1"></script>
-    <script src="js/script.js?v=1.1"></script>
-    <script src="js/carFilter.js?v=1.1"></script>
+    <script src="js/zoom.min.js?v=1.2"></script>
+    <script src="js/formValidationAjax.min.js?v=1.2"></script>
+    <script src="js/script.js?v=1.2"></script>
+    <script src="js/carFilter.js?v=1.2"></script>
   </body>
 </html>
